dc6cbd636d89d56584ba8f6100e30ad6f0f6759f
will-o-the-wisp <kli18@stuy.edu>
2019-02-01 01:20:22 -0500

    added main for testing fib

--- Recursion.java
+++ Recursion.java
@@ -24,6 +24,11 @@ public class Recursion{
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
      *precondition: n is non-negative
      */
+    public static void main(String[] args){
+      for(int i=0;i<7;i++){
+      System.out.print(fib(i)+" ");
+      }
+    }
     public static int fib(int n){
       return fibh(n,1,1,0);
     }
