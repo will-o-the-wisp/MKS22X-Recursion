132e51f09b331f4370d9a99cd70b571dad0035ee
will-o-the-wisp <kli18@stuy.edu>
2019-02-01 00:47:18 -0500

    added skeleton of the class

---
+++ Recursion.java
@@ -0,0 +1,25 @@
+public class Recursion{
+    /*You may write additional private methods */
+
+    /*Recursively find the sqrt using Newton's approximation
+     *tolerance is the allowed percent error the squared answer is away from n.
+     *precondition: n is non-negative
+
+    */
+    public static double sqrt(double n, double tolerance){
+
+    }
+
+    /*Recursively find the n'th fibbonaci number in linear time
+     *fib(0) = 1; fib(1) = 1; fib(5) = 5
+     *precondition: n is non-negative
+     */
+    public static int fib(int n){
+
+    }
+
+    /*As Per classwork*/
+    public static ArrayList<Integer> makeAllSums(){
+    }
+
+}
