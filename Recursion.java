136a0d60885b5f25fdafa49fe3a783abbcd4a551
will-o-the-wisp <kli18@stuy.edu>
2019-02-26 10:53:29 -0500

    debugged with test methods, changed to lowercase

--- Recursion.java
+++ Recursion.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class Recursion{
+public class recursion{
     /*You may write additional private methods */
 
     /*Recursively find the sqrt using Newton's approximation
@@ -7,6 +7,77 @@ public class Recursion{
      *precondition: n is non-negative
 
     */
+    public static boolean closeEnough(double a, double b){
+    if(a==0.0 && b==0.0)return true;
+    if(a==0.0)return b < 0.00000000001;
+    if(b==0.0)return a < 0.00000000001;
+    return Math.abs(a-b)/a < 0.0001;//This is the normal % difference allowed
+
+}
+
+
+//testcase must be a valid index of your input/output array
+public static void testFib(int testcase){
+  recursion r = new recursion();
+  int[] input = {0,1,2,3,5,30};
+  int[] output ={0,1,1,2,5,832040};
+  int max = input.length;
+  if(testcase < input.length){
+    int in = input[testcase];
+    try{
+
+      int ans = r.fib(in);
+      int correct = output[testcase];
+      if(ans == correct){
+        System.out.println("PASS test fib "+in+". "+correct);
+      }
+      else{
+        System.out.println("FAIL test fib"+in+". "+ans+" vs "+correct);
+
+      }
+    }catch(IllegalArgumentException n){
+      if(in < 0){
+        System.out.println("PASS test fib"+in+" IllegalArgumentException");
+      }else{
+        System.out.println(" FAIL IllegalArgumentException in test case:"+in);
+      }
+    }catch(Exception e){
+      System.out.println(" FAIL Some exception in test case:"+in);
+    }
+  }
+}
+
+
+//testcase must be a valid index of your input/output array
+public static void testSqrt(int testcase){
+  recursion r = new recursion();
+  double[] input = {0.0,1.0, 2.0, 4.0, 7.0};
+  double[] output = {0.0,1.0,1.4142135623730951,2.0,2.6457513110645907};
+  int max = input.length;
+  if(testcase < input.length){
+    double in = input[testcase];
+    try{
+
+      double ans = r.sqrt(in,.00001);
+      double correct = Math.sqrt(in);
+      if(closeEnough(ans,correct)){
+        System.out.println("PASS test sqrt "+in+" "+ans);
+      }
+      else{
+        System.out.println("FAIL test sqrt "+in+" "+ans+" vs "+correct);
+
+      }
+    }catch(IllegalArgumentException n){
+      if(in < 0){
+        System.out.println("PASS test sqrt"+in+" IllegalArgumentException");
+      }else{
+        System.out.println(" FAIL IllegalArgumentException in test case:"+in);
+      }
+    }catch(Exception e){
+      System.out.println(" FAIL Some exception in test case:"+in);
+    }
+  }
+}
     public static double sqrt(double n, double tolerance){
       return sqrth(n,tolerance,n);
     }
@@ -27,11 +98,12 @@ public class Recursion{
      */
     public static void main(String[] args){
       for(int i=0;i<5;i++){
-      System.out.print(makeAllSums(i)+" ");
+        testFib(i);
+        testSqrt(i);
       }
     }
     public static int fib(int n){
-      return fibh(n,1,1,0);
+      return fibh(n,0,1,0);
     }
     private static String toString(ArrayList a){
       String ans = "[";
@@ -42,6 +114,9 @@ public class Recursion{
       return ans;
     }
     private static int fibh(int n, int i, int j, int ct){
+      if(n==0){
+        return 0;
+      }
       if(n<2){
         return 1;
       }
